dc667c48607aec38db3bec31d030c7a4aa44f670
Abhishek Kumar <abhi420bhardwaj@gmail.com>
2021-01-20 10:10:31 +0530

    Adding Post Model

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.ForeignKey(
+        'User',
+        on_delete = models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
